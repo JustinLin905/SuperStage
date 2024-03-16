be4990982442ca603b578178508df71a72889862
Justin Lin <tojustinlin@gmail.com>
2024-03-16 18:06:16 -0400

    Initial commit

--- Assets/Scripts/SoundBoard.cs
+++ Assets/Scripts/SoundBoard.cs
@@ -12,6 +12,7 @@ public class SoundBoard : MonoBehaviour
     private GameObject scrollViewContent;
     [SerializeField]
     private Animator animator;
+    private GameObject presenterObj;
     public static GameObject universalAudioSource { get; private set; }
     public bool isShowing { get; private set; }
 
@@ -19,6 +20,7 @@ public class SoundBoard : MonoBehaviour
     {
         // Find GameObject in Scene named "Universal Audiosource"
         universalAudioSource = GameObject.Find("Universal Audiosource");
+        presenterObj = GameObject.Find("PresenterObj");
     }
 
     public void OnPressStop()
@@ -46,6 +48,13 @@ public class SoundBoard : MonoBehaviour
     {
         animator.SetTrigger("Show");
         isShowing = true;
+
+        // Set the position of the sound board to be in front of the presenter
+        transform.position = presenterObj.transform.position + presenterObj.transform.forward * 0.5f - presenterObj.transform.up * 0.25f;
+
+        // Set the rotation of the sound board to face the presenter
+        transform.LookAt(presenterObj.transform);
+        transform.Rotate(-5, 180, 0);
     }
 
     public void Hide()
